bdb000712f12316de6dd4df754ece3ce74a8613f
Ethan Shiota <54644546+Piggggggggggy@users.noreply.github.com>
2022-08-14 01:16:03 -0700

    🐛 fix fahrenheit config not being detected (#4)

--- src/modules/params.rs
+++ src/modules/params.rs
@@ -7,7 +7,10 @@ use crate::location::Geolocation;
 
 pub async fn get(args: &Args, config: &Config) -> Result<Config> {
 	let address = prep_address(args.address.as_deref().unwrap_or_default().to_string(), config).await?;
-	let unit = prep_unit(args.unit.to_string(), config.unit.as_ref())?;
+	let unit = prep_unit(
+		args.unit.as_deref().unwrap_or_default().to_string(),
+		config.unit.as_ref(),
+	)?;
 
 	Ok(Config {
 		address: Some(address),
@@ -27,9 +30,7 @@ async fn prep_address(args_address: String, config: &Config) -> Result<String> {
 	{
 		if args_address.is_empty() {
 			let auto_location_prompt = Confirm::with_theme(&ColorfulTheme::default())
-				.with_prompt(
-					"You didn't specify a city. Should I check for a weather station close to your location?",
-				)
+				.with_prompt("You didn't specify a city. Should I check for a weather station close to your location?")
 				.interact()?;
 			if !auto_location_prompt {
 				std::process::exit(1);
@@ -49,7 +50,7 @@ async fn prep_address(args_address: String, config: &Config) -> Result<String> {
 fn prep_unit(args_unit: String, config_unit: Option<&String>) -> Result<String> {
 	let unit = if args_unit.is_empty() && config_unit.is_some() {
 		match config_unit {
-			unit if unit == Some(&"°F".to_string()) => "fahrenheit",
+			unit if unit == Some(&String::from("fahrenheit")) => "fahrenheit",
 			_ => "celsius",
 		}
 	} else if args_unit == "f" || args_unit == "fahrenheit" {
