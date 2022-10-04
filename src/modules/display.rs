756a970cebea62f09843085d89918b0d5161440e
tobealive <tobmoj@web.de>
2022-10-04 15:46:36 +0200

    🎨 update variables in truncation function
    * names, shadowing

--- src/modules/display.rs
+++ src/modules/display.rs
@@ -37,28 +37,28 @@ impl Product {
 		Ok(())
 	}
 
-	pub fn check_address_len(title: String, max_width: usize) -> Result<String> {
-		let title_len = title.chars().count();
-		let mut new_title = if title_len > max_width {
-			Self::trunc_address(title)?
+	pub fn check_address_len(mut address: String, max_width: usize) -> Result<String> {
+		let address_len = address.chars().count();
+		address = if address_len > max_width {
+			Self::trunc_address(address)?
 		} else {
-			title
+			address
 		};
-		if title_len > max_width {
-			new_title = Self::check_address_len(new_title, max_width)?;
+		if address_len > max_width {
+			address = Self::check_address_len(address, max_width)?;
 		}
-		Ok(new_title)
+		Ok(address)
 	}
 
-	pub fn trunc_address(title: String) -> Result<String> {
-		// let title_commas = title.matches(',').count();
+	pub fn trunc_address(address: String) -> Result<String> {
+		// let address_commas = title.matches(',').count();
 		// For many places with overlong names the results seem better when partially removing text
 		// between first and second comma instead of removing it between penultimate and last comma
 
 		let prep_re = format!("^((?:[^,]*,){{{}}})[^,]*,(.*)", 1);
 		let re = Regex::new(&prep_re).unwrap();
-		let truncated_title = re.replace(&title, "$1$2").to_string();
+		let truncated_address = re.replace(&address, "$1$2").to_string();
 
-		Ok(truncated_title)
+		Ok(truncated_address)
 	}
 }
