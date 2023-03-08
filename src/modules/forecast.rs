80273be97983c9bf132296d3e3e64337cb38966f
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-03-08 01:58:04 +0100

    test: forecast indices

--- src/modules/forecast.rs
+++ src/modules/forecast.rs
@@ -1,3 +1,4 @@
+#[allow(unused_imports)]
 use chrono::{Datelike, Local, Weekday};
 use std::collections::HashSet;
 
@@ -10,8 +11,14 @@ pub fn get_indices(forecast: &HashSet<Forecast>) -> [bool; 9] {
 	// Until there is a more concise solution this is a working and fairly slim approach.
 	let mut forecast_indices = [false; 9];
 	if !forecast.is_empty() {
+		#[cfg(not(test))]
 		let todays_index = Local::now().weekday().number_from_monday();
 
+		#[cfg(test)]
+		// We mock today to always be a Monday, as indices will be dynamically set based on the current day.
+		// If I find a way to mock the system time instead of fixing a specific day, it will be integrated
+		// as preferred method. Nevertheless, we will unit test `get_day_index` using other days as monday.
+		let todays_index = Weekday::Mon.number_from_monday();
 
 		for val in forecast {
 			match val {
@@ -49,3 +56,50 @@ fn get_day_index(todays_index: u32, weekday_index: u32) -> usize {
 	(((weekday_index as i8 - todays_index as i8) % 7 + 7) % 7).try_into().unwrap()
 }
 
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn index_form_day() {
+		// Test to determine the index of the current day when the current day is Monday.
+		assert!(get_day_index(Weekday::Mon.number_from_monday(), Weekday::Tue.number_from_monday()) == 1);
+		assert!(get_day_index(Weekday::Mon.number_from_monday(), Weekday::Wed.number_from_monday()) == 2);
+		assert!(get_day_index(Weekday::Mon.number_from_monday(), Weekday::Thu.number_from_monday()) == 3);
+		assert!(get_day_index(Weekday::Mon.number_from_monday(), Weekday::Fri.number_from_monday()) == 4);
+		assert!(get_day_index(Weekday::Mon.number_from_monday(), Weekday::Sat.number_from_monday()) == 5);
+		assert!(get_day_index(Weekday::Mon.number_from_monday(), Weekday::Sun.number_from_monday()) == 6);
+		// Test to determine the index of the current day when the current day is Saturday.
+		assert!(get_day_index(Weekday::Sat.number_from_monday(), Weekday::Sun.number_from_monday()) == 1);
+		assert!(get_day_index(Weekday::Sat.number_from_monday(), Weekday::Mon.number_from_monday()) == 2);
+		assert!(get_day_index(Weekday::Sat.number_from_monday(), Weekday::Tue.number_from_monday()) == 3);
+		assert!(get_day_index(Weekday::Sat.number_from_monday(), Weekday::Wed.number_from_monday()) == 4);
+		assert!(get_day_index(Weekday::Sat.number_from_monday(), Weekday::Thu.number_from_monday()) == 5);
+		assert!(get_day_index(Weekday::Sat.number_from_monday(), Weekday::Fri.number_from_monday()) == 6);
+	}
+
+	#[test]
+	fn forecast_indices() {
+		assert_eq!(
+			get_indices(&HashSet::from([Forecast::tu, Forecast::we, Forecast::sa])),
+			// [0] = current day; [1..7] = week days; [7] = week overview ; [8] = disable
+			[false, true, true, false, false, true, false, false, false]
+		);
+		assert_eq!(
+			get_indices(&HashSet::from([Forecast::mo])),
+			get_indices(&HashSet::from([Forecast::day])),
+		);
+		assert_eq!(
+			get_indices(&HashSet::from([Forecast::week])),
+			[false, false, false, false, false, false, false, true, false]
+		);
+		assert_eq!(
+			get_indices(&HashSet::from([Forecast::day, Forecast::week])),
+			[true, false, false, false, false, false, false, true, false]
+		);
+		assert_eq!(
+			get_indices(&HashSet::from([Forecast::disable])),
+			[false, false, false, false, false, false, false, false, true]
+		);
+	}
+}
