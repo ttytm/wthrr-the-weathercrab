b92378dc252b3020819f5f286f7d627636a03fa8
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-12-19 20:44:20 +0100

    🎨 format `flake.nix`
    format with https://nixfmt.serokell.io, convert spaces to tabs

--- flake.nix
+++ flake.nix
@@ -1,54 +1,60 @@
 {
-  inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    };
+	inputs = {
+		nixpkgs = {
+			url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+		};
 
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
-  };
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      wthrr-the-weathercrab = {
-        lib,
-        openssl,
-        pkg-config,
-        rustPlatform,
-      }:
-        rustPlatform.buildRustPackage {
-          name = "wthrr-the-weathercrab";
-          src = lib.cleanSource ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-          nativeBuildInputs = [
-            pkg-config
-            rustPlatform.bindgenHook
-          ];
-          buildInputs = [openssl];
+		flake-utils = {
+			url = "github:numtide/flake-utils";
+		};
+	};
+	outputs =
+		inputs@{
+			self,
+			nixpkgs,
+			flake-utils,
+			...
+		}:
+		flake-utils.lib.eachDefaultSystem (
+			system:
+			let
+				pkgs = nixpkgs.legacyPackages.${system};
+				wthrr-the-weathercrab =
+					{
+						lib,
+						openssl,
+						pkg-config,
+						rustPlatform,
+					}:
+					rustPlatform.buildRustPackage {
+						name = "wthrr-the-weathercrab";
+						src = lib.cleanSource ./.;
+						cargoLock.lockFile = ./Cargo.lock;
+						nativeBuildInputs = [
+							pkg-config
+							rustPlatform.bindgenHook
+						];
+						buildInputs = [ openssl ];
 
-          checkFlags = [
-            # connecting to internet does not work in the sandbox
-            "--skip=modules::location::tests::geolocation_response"
-            "--skip=modules::localization::tests::translate_string"
-          ];
-          
-          meta = with lib; {
-            license = licenses.mit;
-            homepage = "https://github.com/ttytm/wthrr-the-weathercrab";
-            platforms = platforms.all;
-          };
-        };
-    in {
-      packages.default = pkgs.callPackage wthrr-the-weathercrab {};
-      apps.default = {
-        type = "app";
-        program = "${self.outputs.packages.${system}.default}/bin/wthrr";
-      };
-    });
+						checkFlags = [
+							# connecting to internet does not work in the sandbox
+							"--skip=modules::location::tests::geolocation_response"
+							"--skip=modules::localization::tests::translate_string"
+						];
+
+						meta = with lib; {
+							license = licenses.mit;
+							homepage = "https://github.com/ttytm/wthrr-the-weathercrab";
+							platforms = platforms.all;
+						};
+					};
+			in
+			{
+				packages.default = pkgs.callPackage wthrr-the-weathercrab { };
+				apps.default = {
+					type = "app";
+					program = "${self.outputs.packages.${system}.default}/bin/wthrr";
+				};
+			}
+		);
 }
