7deb8bccfe0cc100ae14389ebc053f1bc01f04ff
whypeopletho <greyportreit@gmail.com>
2025-10-30 03:06:15 -0400

    clean unneeded function

--- src/modules/json.rs
+++ src/modules/json.rs
@@ -9,11 +9,7 @@ pub async fn to_json(address: &str, language: &str, units: &Units) -> Result<()>
 	let weather = Weather::get(loc.lat, loc.lon, units).await?;
 
 	let data = serde_json::to_string(&weather)?;
-	writing(data);
+	fs::write("test.json", data).expect("failed to write to test.json");
 
 	Ok(())
-}
-
-fn writing(data: String) {
-	fs::write("test.json", data).expect("failed to write to test.json");
 }
